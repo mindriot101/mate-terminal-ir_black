e1a5dc46cf7842c4210359e043e2ffc739447101
Simon Walker <s.r.walker101@googlemail.com>
2014-06-07 17:16:14 +0100

    Adds color scheme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 gnome--terminal-ir_black
 ========================
 
-ir_black theme for gnome terminal
+ir_black theme for mate terminal.
+
+Installation
+------------
+
+Just run 
+
+```
+sh ./ir_black.sh
+```
+
---
+++ ir_black.sh
@@ -0,0 +1,6 @@
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ background-color '#000000000000'
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ bold-color '#000000000000'
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ bold-color-same-as-fg true
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ foreground-color '#FFFFFFFFFFFF'
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ palette '#7C7C7C7C7C7C:#E1E189896464:#0000CDCB0000:#FFFFFFFFB6B6:#66669999CCCC:#FFFF7373FDFD:#9696CDCDFEFE:#EEEEEEEEEEEE:#7C7C7C7C7C7C:#E1E189896464:#A8A8FFFF6060:#FFFFFFFFB6B6:#66669999CCCC:#FFFF7373FDFD:#9696CDCDFEFE:#EEEEEEEEEEEE'
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-theme-colors false
